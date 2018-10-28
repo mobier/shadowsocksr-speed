3f55dc7fdd0c4747d903ba5b4e26895a5345ad49
null <vxxwej@gmail.com>
2018-10-28 23:51:13 +0800

    支持sspanel/v2/v3版本，支持443/常规订阅链接

--- ParseSsr.py
+++ ParseSsr.py
@@ -10,8 +10,17 @@ def fill_padding(base64_encode_str):
 def base64_decode(base64_encode_str):
    base64_encode_str = fill_padding(base64_encode_str)
    return base64.urlsafe_b64decode(base64_encode_str).decode('utf-8')
-
-def parse(base64_encode_str):
+def parse(base64_encode_str): #
+   decode_str = base64_decode(base64_encode_str)
+   parts = decode_str.split(':')
+   if len(parts) != 6:
+       print('不能解析SSR链接: %s' % base64_encode_str)
+       return
+   if parts[1]=="443" or parts[1]=="444":
+      return parse_ssr(base64_encode_str)
+   else:
+      return parse_ssr(base64_encode_str)
+def parse_ssr_433(base64_encode_str):
    decode_str = base64_decode(base64_encode_str)
    #print(decode_str)
    parts = decode_str.split(':')
@@ -41,6 +50,7 @@ def parse(base64_encode_str):
        param_dic[key_and_value[0]] = key_and_value[1]
 
    obfsparam = base64_decode(param_dic['obfsparam'])
+   protoparam = base64_decode(param_dic['protoparam'])
    remarks = base64_decode(param_dic['remarks'])
    group = base64_decode(param_dic['group'])
 
@@ -54,10 +64,62 @@ def parse(base64_encode_str):
    ss_result['obfsparam']=obfsparam
    ss_result['remarks']=remarks
    ss_result['group']=group
+   ss_result['protoparam']=protoparam
+
    # print('server: %s, port: %s, 协议: %s, 加密方法: %s, 密码: %s, 混淆: %s, 混淆参数: %s,  备注: %s, 分组: %s'
    #       % (server, port, protocol, method, password, obfs, obfsparam, remarks, group))
    #print(ss_result)
    return ss_result
+
+
+def parse_ssr(base64_encode_str):
+   decode_str = base64_decode(base64_encode_str)
+   parts = decode_str.split(':')
+   if len(parts) != 6:
+       print('不能解析SSR链接: %s' % base64_encode_str)
+       return
+
+   server = parts[0]
+   port = parts[1]
+   protocol = parts[2]
+   method = parts[3]
+   obfs = parts[4]
+   password_and_params = parts[5]
+   password_and_params = password_and_params.split("/?")
+   password_encode_str = password_and_params[0]
+   password = base64_decode(password_encode_str)
+   params = password_and_params[1]
+   param_parts = params.split('&')
+
+   param_dic = {}
+   ss_result={}
+   for part in param_parts:
+       key_and_value = part.split('=')
+       param_dic[key_and_value[0]] = key_and_value[1]
+
+   obfsparam = base64_decode(param_dic['obfsparam'])
+   if('protoparam' in param_dic):
+      protoparam = base64_decode(param_dic['protoparam'])
+      ss_result['protoparam']=protoparam
+   remarks = base64_decode(param_dic['remarks'])
+   group = base64_decode(param_dic['group'])
+
+   ss_result['server']=server
+   ss_result['port']=port
+   ss_result['protocol']=protocol
+   ss_result['method']=method
+   ss_result['password']=password
+   ss_result['obfs']=obfs
+   ss_result['obfsparam']=obfsparam
+   ss_result['remarks']=remarks
+   ss_result['group']=group
+
+
+   return ss_result
+   # print("解析结果:")
+   # print('server: %s, port: %s, 协议: %s, 加密方法: %s, 密码: %s, 混淆: %s, 混淆参数: %s, 协议参数: %s, 备注: %s, 分组: %s'
+   #       % (server, port, protocol, method, password, obfs, obfsparam, protoparam, remarks, group))
+
  #python /home/nu11/.config/electron-ssr/shadowsocksr/shadowsocks/local.py -s hk2.pyjiaoyi.cf -p 10384 -k EoWjVthG -m chacha20-ietf -O auth_aes128_md5 -o http_simple -g download.windowsupdate.com -b 0.0.0.0 -l 1080
  #python3 shadowsocksr/shadowsocks/local.py -s us1.pyjiaoyi.cf -p 10384 -k EoWjVthG -m chacha20-ietf -O auth_aes128_md5 -o http_simple -g download.windowsupdate.com -b 0.0.0.0 -l 6666
 #s hk2.pyjiaoyi.cf -p 10384 -k EoWjVthG -m chacha20-ietf -O auth_aes128_md5 -o http_simple -g download.windowsupdate.com -b 0.0.0.0 -l 1080
--- shadowsocksr-speed.py
+++ shadowsocksr-speed.py
@@ -5,12 +5,26 @@ import os
 import time
 import requests
 import ParseSsr #https://www.jianshu.com/p/81b1632bea7f
+import re
 from prettytable import PrettyTable
+
+
+only_check_network=False
+
+def isIP(str):
+    p = re.compile('^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$')
+    if p.match(str):
+        return True
+    else:
+        return False
 
 def connect_ssr(ssr):
   try:
     port="6667"
-    cmd="python shadowsocksr/shadowsocks/local.py -qq -s %s -p %s -k %s -m %s -O %s -o %s -g %s -b %s -l %s" % (ssr['server'],ssr['port'],ssr['password'],ssr['method'],ssr['protocol'],ssr['obfs'],ssr['obfsparam'],"127.0.0.1",port)
+    if('protoparam' in ssr):
+        cmd="python shadowsocksr/shadowsocks/local.py -qq -s %s -p %s -k %s -m %s -O %s -G %s -o %s -g %s -b %s -l %s" % (ssr['server'],ssr['port'],ssr['password'],ssr['method'],ssr['protocol'],ssr['protoparam'],ssr['obfs'],ssr['obfsparam'],"127.0.0.1",port)
+    else:
+        cmd="python shadowsocksr/shadowsocks/local.py -qq -s %s -p %s -k %s -m %s -O %s -o %s -g %s -b %s -l %s" % (ssr['server'],ssr['port'],ssr['password'],ssr['method'],ssr['protocol'],ssr['obfs'],ssr['obfsparam'],"127.0.0.1",port)
     os.system(cmd + " -d stop")
     os.system(cmd + " -d start")
 
@@ -20,34 +34,44 @@ def connect_ssr(ssr):
     import socks
     socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", int(port))
     socket.socket = socks.socksocket
-    ip=requests.get('http://api.ip.sb/ip',timeout=30).text
+    ip=requests.get('http://api.ip.sb/ip',timeout=15).text
 
     result={}
     result['host']=ssr['server']
     result['remarks']=ssr['remarks']
     result['ip']=ip.strip()
     print(result['ip'])
-    cmd="ping -c 1 %s |grep 'time=' | awk '{print $8}' |cut -b 6-"% result['host']
+    if isIP(result['host']):
+        ping_len="7"
+    else:
+        ping_len="8"
+    cmd="ping -c 1 %s |grep 'time=' | awk '{print $%s}' |cut -b 6-"% (result['host'],ping_len)
+
     ping_pc=os.popen(cmd).readlines()#.strip()
     if(len(ping_pc)):
       ping_pc=ping_pc[0].strip()
     print("ping_pc",ping_pc)
-
-    import speedtest
-    s = speedtest.Speedtest()
-    s.get_best_server()
-    s.download()
-    s.upload()
-    s.results.share()
-    results_dict = s.results.dict()
     result['ping_pc']=ping_pc
-    result['ping']=results_dict['ping']
-    result['download']=round(results_dict['download'] / 1000.0 / 1000.0,2)
-    result['upload']=round(results_dict['upload'] / 1000.0 / 1000.0 ,2)
-    result['action']="Success"
-    print(result['ping'],"/",result['ping_pc'])
-    print(result['download'],"Mbit/s")
-    print(result['upload'],"Mbit/s")
+    if not only_check_network:
+        import speedtest
+        s = speedtest.Speedtest()
+        s.get_best_server()
+        s.download()
+        s.upload()
+        s.results.share()
+        results_dict = s.results.dict()
+        result['ping']=results_dict['ping']
+        result['download']=round(results_dict['download'] / 1000.0 / 1000.0,2)
+        result['upload']=round(results_dict['upload'] / 1000.0 / 1000.0 ,2)
+        result['state']="Success"
+        print(result['ping'],"/",result['ping_pc'])
+        print(result['download'],"Mbit/s")
+        print(result['upload'],"Mbit/s")
+    else:
+        result['download']=0
+        result['upload']=0
+        result['ping']=0
+        result['state']="Success"
     return result
 
   except Exception as e:
@@ -58,25 +82,26 @@ def connect_ssr(ssr):
     result['download']=0
     result['upload']=0
     result['ping']=0
-    result['action']="Fail"
+    result['state']="Fail"
     cmd="ping -c 1 %s |grep 'time=' | awk '{print $8}' |cut -b 6-"% result['host']
     ping_pc=os.popen(cmd).readlines()#.strip()
     if(len(ping_pc)):
       ping_pc=ping_pc[0].strip()
     result['ping_pc']=ping_pc
+    print("ping_local",ping_pc)
     print (e)
-
     return result
-    
 
 url=input("url:")
 ssr_config=[]
 speed_result=[]
-headers = {'User-Agent': 'User-Agent:Mozilla/5.0'}
+headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
 f=urllib.request.Request(url,headers=headers) 
-ssr_subscribe = urllib.request.urlopen(f).read() #获取ssr订阅链接中数据
-ssr_subscribe_decode = base64.b64decode(ssr_subscribe).decode('utf-8')
-ssr_subscribe_decode=ssr_subscribe_decode.split('\r\n')
+
+ssr_subscribe = urllib.request.urlopen(f).read().decode('utf-8') #获取ssr订阅链接中数据
+ssr_subscribe_decode = ParseSsr.base64_decode(ssr_subscribe)
+ssr_subscribe_decode=ssr_subscribe_decode.replace('\r','')
+ssr_subscribe_decode=ssr_subscribe_decode.split('\n')
 
 for i in ssr_subscribe_decode:
 	if(i):
@@ -84,13 +109,20 @@ for i in ssr_subscribe_decode:
 		ssr_config.append(ParseSsr.parse(decdata))#解析"SSR://" 后边的base64的配置信息返回一个字典
 for s in ssr_config:
   speed_result.append(connect_ssr(s))#通过解析后的配置信息链接节点进行测速
-
+speed_result.append(connect_ssr(ssr_config[4]))#通过解析后的配置信息链接节点进行测速
 
 #将测速结果生产为表格
 table=PrettyTable(["name","ip","localPing","ping","upload","download"])
 table.sortby = "download"#以"download"下载速度为排序根据
 table.reversesort = True
 for t in speed_result:
-  table.add_row([t['remarks'],t['ip'],t['ping_pc'],t['ping'],t['upload'],t['download']])
+    table.add_row([t['remarks'],t['ip'],t['ping_pc'],t['ping'],t['upload'],t['download']])
 print(table)
+
+
+# for s in ssr_config:
+#   speed_result=connect_ssr(s)#通过解析后的配置信息链接节点进行测速
+#   print(speed_result)
+#   table.add_row([speed_result['remarks'],speed_result['ip'],speed_result['ping_pc'],speed_result['ping'],speed_result['upload'],speed_result['download']])
+#   print(table)
 
