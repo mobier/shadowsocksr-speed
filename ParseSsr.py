9a06818f8cba737f99c04e6405af4893c7ac8f06
BeanWei <157318439@qq.com>
2018-11-12 19:01:51 +0800

    完善优化SSR链接解析的代码部分, 并添加测试用例

--- ParseSsr.py
+++ ParseSsr.py
@@ -1,125 +1,96 @@
 import base64
 
-def fill_padding(base64_encode_str):
-   need_padding = len(base64_encode_str) % 4
-   if need_padding != 0:
-       missing_padding = 4 - need_padding
-       base64_encode_str += '=' * missing_padding
-   return base64_encode_str
+#python /home/nu11/.config/electron-ssr/shadowsocksr/shadowsocks/local.py -s hk2.pyjiaoyi.cf -p 10384 -k EoWjVthG -m chacha20-ietf -O auth_aes128_md5 -o http_simple -g download.windowsupdate.com -b 0.0.0.0 -l 1080
+#python3 shadowsocksr/shadowsocks/local.py -s us1.pyjiaoyi.cf -p 10384 -k EoWjVthG -m chacha20-ietf -O auth_aes128_md5 -o http_simple -g download.windowsupdate.com -b 0.0.0.0 -l 6666
+#s hk2.pyjiaoyi.cf -p 10384 -k EoWjVthG -m chacha20-ietf -O auth_aes128_md5 -o http_simple -g download.windowsupdate.com -b 0.0.0.0 -l 1080
+
 
 def base64_decode(base64_encode_str):
-   base64_encode_str = fill_padding(base64_encode_str)
-   return base64.urlsafe_b64decode(base64_encode_str).decode('utf-8')
-def parse(base64_encode_str): #
-   decode_str = base64_decode(base64_encode_str)
-   parts = decode_str.split(':')
-   if len(parts) != 6:
-       print('不能解析SSR链接: %s' % base64_encode_str)
-       return
-   if parts[1]=="443" or parts[1]=="444":
-      return parse_ssr(base64_encode_str)
-   else:
-      return parse_ssr(base64_encode_str)
-def parse_ssr_433(base64_encode_str):
-   decode_str = base64_decode(base64_encode_str)
-   #print(decode_str)
-   parts = decode_str.split(':')
-   if len(parts) != 6:
-       print('不能解析SSR链接: %s' % base64_encode_str)
-       return
-   server = parts[0]
-   port = parts[1]
-   protocol = parts[2]
-   method = parts[3]
-   obfs = parts[4]
+    """ 利用 base64.urlsafe_b64decode 对字符串解码 """
+    
+    if base64_encode_str:
+        need_padding = len(base64_encode_str) % 4
+        if need_padding != 0:
+            missing_padding = 4 - need_padding
+            base64_encode_str += '=' * missing_padding
+        return base64.urlsafe_b64decode(base64_encode_str).decode('utf-8')
+    return base64_encode_str
 
-   password_and_params = parts[5]
-   password_and_params = password_and_params.split("/?")
-   params = password_and_params[1]
+def parse(ssrUrl):
+    """ 解析ssr链接 
+    args: 
+        ssrUrl: SSR 链接
+    return: 
+        ssr_result: SSR链接解析结果
+    """
 
-   param_parts = params.split('&')
-   password_encode_str = param_parts[1]
-   password_encode_str=password_encode_str.split('=')[1]
-   password = base64_decode(password_encode_str)
-   port=password.split(':')[0]
-   password=password.split(':')[1]
+    ssr_result = {}
 
-   param_dic = {}
-   for part in param_parts:
-       key_and_value = part.split('=')
-       param_dic[key_and_value[0]] = key_and_value[1]
+    decode_str = base64_decode(ssrUrl)
+    parts = decode_str.split(':')
+    if len(parts) != 6:
+        print('不能解析SSR链接: %s' % ssrUrl)
+        return
+    
+    server = parts[0]
+    port = parts[1]
+    protocol = parts[2]
+    method = parts[3]
+    obfs = parts[4]
 
-   obfsparam = base64_decode(param_dic['obfsparam'])
-   protoparam = base64_decode(param_dic['protoparam'])
-   remarks = base64_decode(param_dic['remarks'])
-   group = base64_decode(param_dic['group'])
+    password_and_params = parts[5]
+    password_and_params = password_and_params.split("/?")
+    password = base64_decode(password_and_params[0])
+    if len(password_and_params) == 1:
+        obfsparam = ""
+        protoparam = ""
+        remarks = ""
+        group = ""
+    else:
+        param_dic = {}
+        param_parts = password_and_params[1].split('&')
+        for part in param_parts:
+            key_and_value = part.split('=')
+            param_dic[key_and_value[0]] = key_and_value[1]
 
-   ss_result={}
-   ss_result['server']=server
-   ss_result['port']=port
-   ss_result['protocol']=protocol
-   ss_result['method']=method
-   ss_result['password']=password
-   ss_result['obfs']=obfs
-   ss_result['obfsparam']=obfsparam
-   ss_result['remarks']=remarks
-   ss_result['group']=group
-   ss_result['protoparam']=protoparam
+        obfsparam = base64_decode(param_dic.get('obfsparam', ""))
+        protoparam = base64_decode(param_dic.get('protoparam', ""))
+        remarks = base64_decode(param_dic.get('remarks', ""))
+        group = base64_decode(param_dic.get('group', ""))
+        
+    ssr_result['server']=server
+    ssr_result['port']=port
+    ssr_result['protocol']=protocol
+    ssr_result['method']=method
+    ssr_result['password']=password
+    ssr_result['obfs']=obfs
+    ssr_result['obfsparam']=obfsparam
+    ssr_result['remarks']=remarks
+    ssr_result['group']=group
+    ssr_result['protoparam']=protoparam
 
-   # print('server: %s, port: %s, 协议: %s, 加密方法: %s, 密码: %s, 混淆: %s, 混淆参数: %s,  备注: %s, 分组: %s'
-   #       % (server, port, protocol, method, password, obfs, obfsparam, remarks, group))
-   #print(ss_result)
-   return ss_result
-
-
-def parse_ssr(base64_encode_str):
-   decode_str = base64_decode(base64_encode_str)
-   parts = decode_str.split(':')
-   if len(parts) != 6:
-       print('不能解析SSR链接: %s' % base64_encode_str)
-       return
-
-   server = parts[0]
-   port = parts[1]
-   protocol = parts[2]
-   method = parts[3]
-   obfs = parts[4]
-   password_and_params = parts[5]
-   password_and_params = password_and_params.split("/?")
-   password_encode_str = password_and_params[0]
-   password = base64_decode(password_encode_str)
-   params = password_and_params[1]
-   param_parts = params.split('&')
-
-   param_dic = {}
-   ss_result={}
-   for part in param_parts:
-       key_and_value = part.split('=')
-       param_dic[key_and_value[0]] = key_and_value[1]
-
-   obfsparam = base64_decode(param_dic['obfsparam'])
-   if('protoparam' in param_dic):
-      protoparam = base64_decode(param_dic['protoparam'])
-      ss_result['protoparam']=protoparam
-   remarks = base64_decode(param_dic['remarks'])
-   group = base64_decode(param_dic['group'])
-
-   ss_result['server']=server
-   ss_result['port']=port
-   ss_result['protocol']=protocol
-   ss_result['method']=method
-   ss_result['password']=password
-   ss_result['obfs']=obfs
-   ss_result['obfsparam']=obfsparam
-   ss_result['remarks']=remarks
-   ss_result['group']=group
+    return ssr_result
 
 
-   return ss_result
-   # print("解析结果:")
-   # print('server: %s, port: %s, 协议: %s, 加密方法: %s, 密码: %s, 混淆: %s, 混淆参数: %s, 协议参数: %s, 备注: %s, 分组: %s'
-   #       % (server, port, protocol, method, password, obfs, obfsparam, protoparam, remarks, group))
+if __name__ == "__main__":
+    test_ssrUrl_list = [
+        "ssr://MTQyLjkzLjg0LjE1Mzo4ODgwOmF1dGhfY2hhaW5fYTpub25lOnBsYWluOmJYbGFha0kxYWtoMVpuaFZNRGhLWWxkd1lYTnpkMjl5WkE",
 
- #python /home/nu11/.config/electron-ssr/shadowsocksr/shadowsocks/local.py -s hk2.pyjiaoyi.cf -p 10384 -k EoWjVthG -m chacha20-ietf -O auth_aes128_md5 -o http_simple -g download.windowsupdate.com -b 0.0.0.0 -l 1080
- #python3 shadowsocksr/shadowsocks/local.py -s us1.pyjiaoyi.cf -p 10384 -k EoWjVthG -m chacha20-ietf -O auth_aes128_md5 -o http_simple -g download.windowsupdate.com -b 0.0.0.0 -l 6666
-#s hk2.pyjiaoyi.cf -p 10384 -k EoWjVthG -m chacha20-ietf -O auth_aes128_md5 -o http_simple -g download.windowsupdate.com -b 0.0.0.0 -l 1080
+        "ssr://NTAuMy4yNDIuMTMzOjEyNTE0OmF1dGhfc2hhMV92NDpjaGFjaGEyMDpodHRwX3NpbXBsZTpOalUwTnprLz9vYmZzcGFyYW09JnByb3RvcGFyYW09JnJlbWFya3M9Nzd5SU1URXVNVEh2dklubGhZM290TG5tdFl2b3I1WG9pb0xuZ3JrSzZabVE2WUNmTXpBd2EySXZjLW1jZ09pbWdlbXJtT21Bbi1lYWhPaUtndWVDdVZGUk1USXdPREU0TmpreU53Jmdyb3VwPQ",
+
+        "ssr://OTEuMTkyLjgxLjMxOjgwOmF1dGhfY2hhaW5fYTpub25lOmh0dHBfc2ltcGxlOllXUnRhVzVoWkcxcGJtRmtiV2x1Lz9vYmZzcGFyYW09JnByb3RvcGFyYW09TVRweGNYRjNaR1kmcmVtYXJrcz01TC1FNVp1OSZncm91cD0",
+
+        "ssr://MTY3LjE3OS43NC4zNDoyNzcwMTphdXRoX2NoYWluX2E6bm9uZTpwbGFpbjpPRGs0TkRJNGFHaGtjZy8_b2Jmc3BhcmFtPSZwcm90b3BhcmFtPSZyZW1hcmtzPTZMU3Q1TG13NVlxZ1VWRTFNVFl3TkRVNEN1LThpT2F0cE9XSWh1UzZxLW1aa09tQW4tLThpUSZncm91cD0",
+
+        "ssr://MTc2LjMyLjM1LjI1NDoyNTAzMTphdXRoX2NoYWluX2E6YWVzLTI1Ni1jZmI6cGxhaW46TmpVME16SXgvP29iZnNwYXJhbT0mcHJvdG9wYXJhbT0mcmVtYXJrcz1NVGMyTGpNeUxqTTFMakkxTkEmZ3JvdXA9",
+
+        "ssr://MTM5LjE4MC4yMTMuMjE6MTY0Mjc6b3JpZ2luOmFlcy0yNTYtY2ZiOnBsYWluOk1XTlZSR1kxLz9vYmZzcGFyYW09JnByb3RvcGFyYW09JnJlbWFya3M9YUhWcExXcHBMbmg1ZXVhenFPV0dqT21BZ1RFd1ItYTFnZW1Iai1hV3NPV0tvT1dkb1EmZ3JvdXA9NTRHdzVweTY",
+
+        "ssr://MTc2LjMyLjM1LjI1NDoyNTAzMTpvcmlnaW46YWVzLTI1Ni1jZmI6cGxhaW46TmpVME16SXg"
+    ]
+    print("解析结果:\n*===========================*")
+    for url in test_ssrUrl_list:
+        ssr = parse(url[6:])
+        print(' server: %s\n port: %s\n 协议: %s\n 加密方法: %s\n 密码: %s\n 混淆: %s\n 混淆参数: %s\n 协议参数: %s\n 备注: %s\n 分组: %s'
+          % (ssr["server"], ssr["port"], ssr["protocol"], ssr["method"], ssr["password"], ssr["obfs"], ssr["obfsparam"], ssr["protoparam"], ssr["remarks"], ssr["group"]))
+        print("*===========================*")
